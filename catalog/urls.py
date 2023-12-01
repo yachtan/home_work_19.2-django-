64da473f68bf9d61fd47c906bf5f27c6a2542601
yachanova_tania <tan9l_85@mail.ru>
2023-12-01 23:57:44 +0300

    hw_20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, catalog, all_products, product
+from catalog.apps import CatalogConfig
 
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('catalog/', catalog, name='catalog'),
+    path('<int:pk>/all_products/', all_products, name='all_products'),
+    path('<int:pk>/product/', product, name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product, Category
 
 # Create your views here.
 
@@ -9,11 +10,41 @@ def home(request):
     return render(request, 'catalog/home.html')
 
 
+def catalog(request):
+    category_list = Category.objects.all()
+    context = {
+        'object_list': category_list,
+        'title': 'Категории товаров'
+    }
+    return render(request, 'catalog/catalog.html', context)
+
+
+def all_products(request, pk):
+    product_list = Product.objects.filter(category=pk)
+    context = {
+        'object_list': product_list,
+        'title': 'Все продукты категории'
+    }
+    return render(request, 'catalog/all_products.html', context)
+
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk),
+        'title': 'О продукте'
+    }
+    return render(request, 'catalog/product.html', context)
+
+
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == 'POST':
         # в переменной request хранится информация о методе, который отправлял пользователь
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(name, phone, message)
-    return render(request, 'catalog/contacts.html')
+
+    return render(request, 'catalog/contacts.html', context)
